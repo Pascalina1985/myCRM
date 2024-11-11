084d67b555b79bb6098163a5666197db08a7452f
Pascal Megnin <136910002+Pascalina1985@users.noreply.github.com>
2024-11-11 16:53:12 +0100

    added detailed user component

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Firestore, doc, docData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { DocumentData } from '@firebase/firestore';
+import { User } from '../../models/user.class';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatIconModule],
   templateUrl: './user-detail.component.html',
-  styleUrl: './user-detail.component.scss'
+  styleUrls: ['./user-detail.component.scss']
 })
-export class UserDetailComponent {
+export class UserDetailComponent implements OnInit {
+  firestore: Firestore = inject(Firestore);
+  userId = '';
+  user$: Observable<DocumentData | undefined> | undefined;
+  user: User = new User();  
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.userId = params['id'];
+      this.getUser();
+      });
+  }
+
+  getUser() {
+    const userDocRef = doc(this.firestore, `users/${this.userId}`);
+    this.user$ = docData(userDocRef, { idField: 'id' });
+    
+    this.user$.subscribe((user) => {
+      this.user = new User(user);
+      });
+  }
+  openAdressDialog(){}
 }
